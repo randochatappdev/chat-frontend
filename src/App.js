1b0f242a5dcd86523d9d04c475211ab010b21d99
Mark Jabel Frias <friasmarkjabel@gmail.com>
2021-03-21 12:34:14 +0800

    Setup router for the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* This div is only for testing purposes */}
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/welcome">Welcome</Link>
+          </li>
+          <li>
+            <Link to="/exit">Exit</Link>
+          </li>
+
+        </ul>
+      </div>
+
+
+      <Switch>
+
+
+        <Route path="/welcome">
+          <Hello></Hello>
+        </Route>
+
+        <Route path="/exit">
+          <Goodbye></Goodbye>
+        </Route>
+
+        <Route path="/">
+          <p>Home</p>
+        </Route>
+
+
+      </Switch>
+    </Router>
   );
 }
+
+/* The following components are for testing purposes only. They are placeholders. */
+function Hello() {
+  return (
+    <p>Hello!</p>
+  )
+}
+
+function Goodbye() {
+  return (
+    <p>Bye!</p>
+  )
+}
+
+
 
 export default App;
