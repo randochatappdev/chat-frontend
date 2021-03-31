3eab7615fbc320cdc917352a7dec9146e785526e
Kenneth <kennfamarin@gmail.com>
2021-03-31 18:10:36 +0800

    add testing

---
+++ src/components/chat-ui/chat.js
@@ -0,0 +1,9 @@
+import CallIcon from '@material-ui/icons/Call';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import SettingsIcon from '@material-ui/icons/Settings';
+
+function chat() {
+    return(
+        CallIcon 
+    )
+}
