d430f35f9d76d0107db37807a9be05a0daa0a89a
Kenneth <kennfamarin@gmail.com>
2021-04-27 06:10:54 +0800

    chat-attach

--- src/components/chat-ui/chat.js
+++ src/components/chat-ui/chat.js
@@ -9,6 +9,8 @@ import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
+import { DropzoneDialog } from 'material-ui-dropzone';
+import React, { useState } from "react";
 
 const useStyles = makeStyles((theme) => ({
 
@@ -22,6 +24,7 @@ const useStyles = makeStyles((theme) => ({
 
 function Chat() {
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
     return (
 
         <div className="chat-wrapper">
@@ -33,8 +36,8 @@ function Chat() {
             </div>
 
             <span className="helo">
-                <p className="send">That's dope haha Only by cutting off the connection would she be able to free herself from the beckoning of the Chaos Abyss.Only by cutting off the connection would she be able to free herself from the beckoning of the Chaos Abyss.</p>
-                <p className="reply">Nope. That's weird Only by cutting off the connection would she be able to free herself from the beckoning of the Chaos Abyss.Only by cutting off the connection would she be able to free herself from the beckoning of the Chaos Abyss.</p>
+                <p className="send">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p className="reply">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 <p className="send">Well, you're right hahaha</p>
                 <p className="reply">Here, another one hahahah</p>
                 <p className="send">Now, that's a weird one</p>
@@ -42,14 +45,32 @@ function Chat() {
 
             <div className="chat-text">
                 <CallIcon className="call-icons" />
-                <AttachFileIcon className="attach-icons" />
+
+                <div>
+                <AttachFileIcon className="attach-icons" onClick={() => setOpen(true)} />
+                <DropzoneDialog
+                    acceptedFiles={['image/*']}
+                    cancelButtonText={"cancel"}
+                    submitButtonText={"submit"}
+                    maxFileSize={5000000}
+                    open={open}
+                    onClose={() => setOpen(false)}
+                    onSave={(files) => {
+                        console.log('Files:', files);
+                        setOpen(false);
+                        }}
+                    showPreviews={true}
+                    showFileNamesInPreview={true}
+                />
+                </div>
+
                 <form>
                     <TextField className={clsx(classes.textField)} id="outlined-basic" label="Type your message here" variant="outlined" />
                 </form>
                 <SendIcon className="send-icons" />
             </div>
-
         </div>
+        
     )
 }
 
