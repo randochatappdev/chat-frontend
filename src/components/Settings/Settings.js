189d6563884bf57e6365fe7e07d4e5deee138434
Mark Jabel Frias <friasmarkjabel@gmail.com>
2021-05-01 19:32:50 +0800

    Add interaction logic to Settings components

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,37 +1,89 @@
 import './Settings.css';
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+function mapStateToProps(state) {
+    const { currentUser } = state;
+    return { currentUser };
+}
 
 
-function Settings (props)  {
-    return(
-        <div className="container">
-            
-            <h1 className="header">Settings</h1>
-            
-            <div className="list">
-                <h2 className="account">ACCOUNT</h2>
 
-                <p className="property-header">Name</p>
-                <p className="property-value">Helen Stevens</p>
+function Settings(props) {
+    return (
+        // Show components when currentUser is fetched. Otherwise, render loading screen
 
-                <p className="property-header">Alias</p>
-                <p className="property-value">Karmachemeleon</p>
+        props.currentUser
 
-                <p className="property-header">Birthday</p>
-                <p className="property-value">January 01,1991</p>
+            ?
+            <div className="container">
 
-                <p className="property-header">Gender</p>
-                <p className="property-value">Male</p>
+                <h1 className="header">Settings</h1>
+
+                <div className="list">
+                    <h2 className="account">ACCOUNT</h2>
+
+                    <p className="property-header">Name</p>
+                    <p className="property-value">{`${props.currentUser.firstName} ${props.currentUser.lastName}`}</p>
+
+                    <p className="property-header">Alias</p>
+                    <p className="property-value">{props.currentUser.alias}</p>
+
+                    {/*<p className="property-header">Birthday</p>
+                    <p className="property-value">{props.currentUser.</p>*/}
+
+                    <p className="property-header">Gender</p>
+                    <p className="property-value">{props.currentUser.gender}</p>
+
+                </div>
+
+                <Buttons />
+
+
 
             </div>
-           
 
-            <div className="button">
-                <Button color="secondary">Logout</Button>
-                <Button color="primary">Manage Topic Preferences</Button>
+            :
+            <div className="loading-container">
+                <CircularProgress className="loading" />
+
             </div>
-        </div>
+
     )
 }
-export default Settings;
+
+function Buttons(props) {
+    let history = useHistory();
+
+    function handleLogOut() {
+        const localStorage = window.localStorage;
+
+        // Remove stored data in local storage
+        localStorage.removeItem('jwt');
+        localStorage.removeItem('sessionToken');
+
+        // Redirect to login page
+        history.push('/login')
+
+        // Reload page
+        window.location.reload();
+    }
+
+    function handleClickTopicPref() {
+        history.push('/topics/edit')
+
+    }
+
+    return (
+        <div className="button">
+            <Button color="secondary" onClick={handleLogOut}>Logout</Button>
+            <Button color="primary" onClick={handleClickTopicPref}>Manage Topic Preferences</Button>
+        </div>
+    )
+
+
+}
+export default connect(mapStateToProps)(Settings);
